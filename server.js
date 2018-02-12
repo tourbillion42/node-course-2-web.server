2a0613ba85121c38212f503b6fdb3d4d838802ac
tourbillion42 <tourbillion42@gmail.com>
2018-02-12 19:44:13 +0900

    Setup start script and heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 9000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -11,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
-  
+
   console.log(log);
   fs.appendFile('server.log', log + '\n', (err) => {
     if (err) {
@@ -46,6 +47,6 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.listen(9000, () => {
-  console.log('Server running!');
+app.listen(port, () => {
+  console.log(`Server running! ${port}`);
 })
